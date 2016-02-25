788a5801780c13c3947b8fb41301b0b0ba12b8a0
Sergey Kalitin <sergey.kalitin@gmail.com>
2016-02-25 17:02:57 +0300

    first commit

---
+++ Fibonacci.Console/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Fibonacci.Console
+{
+    static class Program
+    {
+        public static void Main(string[] arguments)
+        {
+            System.Console.WriteLine("Test");
+        }
+    }
+    
+}
